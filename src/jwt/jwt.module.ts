2db78ea3ad9c0892393200c113135caeca63fd95
DevYuns <ydhun93@gmail.com>
2021-03-22 02:03:57 +0900

    [Jwt]Jwt module & service & custom static method

---
+++ src/jwt/jwt.module.ts
@@ -0,0 +1,22 @@
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { Module, DynamicModule, Global } from '@nestjs/common';
+import { JwtService } from './jwt.service';
+
+@Module({})
+@Global()
+export class JwtModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
+    return {
+      module: JwtModule,
+      exports: [JwtService],
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        JwtService,
+      ],
+    };
+  }
+}
---
+++ src/jwt/jwt.service.ts
@@ -0,0 +1,14 @@
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
+import * as jwt from 'jsonwebtoken';
+import { Injectable, Inject } from '@nestjs/common';
+
+@Injectable()
+export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
+  sign(userId: number): string {
+    return jwt.sign({ id: userId }, this.options.secretKey);
+  }
+}
