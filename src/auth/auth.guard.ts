37f544dfc6f4385a643dfec40073ef4214e87b56
DevYuns <ydhun93@gmail.com>
2021-05-05 21:35:37 +0900

    Refactoring Atuh guard to get a token from both http & subscription

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,27 +1,43 @@
+import { JwtService } from './../jwt/jwt.service';
 import { User } from './../users/entities/user.entity';
 import { allowedRoles } from './role.decorator';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { Reflector } from '@nestjs/core';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<allowedRoles>(
       'roles',
       context.getHandler(),
     );
+
     if (!roles) {
       return true;
     }
 
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user: User = gqlContext['user'];
-    if (!user) {
-      return false;
+    const token = gqlContext.token;
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+        if (user) {
+          gqlContext['user'] = user;
+          if (roles.includes('Any')) {
+            return true;
+          }
+          return roles.includes(user.role);
+        }
+      }
     }
-
-    return roles.includes('Any') || roles.includes(user.role);
+    return false;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
+import { UsersModule } from './../users/users.module';
 import { AuthGuard } from './auth.guard';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 @Module({
+  imports: [UsersModule],
   providers: [
     {
       provide: APP_GUARD,
